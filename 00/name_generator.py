3156e7e22cd2dbe0b499d7c729bd5ddff19b289f
Boris Zolotukhin <bzolotukhin@luxoft.com>
2018-07-18 13:35:09 +0600

    Blanck files for task #00 And added some help message.

---
+++ 00/name_generator.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+	pass
+
+
+
+if __name__ == "__main__":
+
+	if (len(sys.argv) > 1 ) and any(sys.argv[1] in s for s in ['--help','-h','/?']):
+	    print("Display a list of randomly generated names. \nUsage:\t name_generator.py [-c <number of names>] \n\t -c <number of names>\t Default number of names is five.")
+    	exit(0)
+
+	main()

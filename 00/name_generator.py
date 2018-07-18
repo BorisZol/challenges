a87b86a5b920a5de240c42eba758794cb44db76b
BorisZol <bzolotuhin@gmail.com>
2018-07-18 22:25:08 +0600

    Cyrillic in windows

--- 00/name_generator.py
+++ 00/name_generator.py
@@ -7,8 +7,11 @@ def main():
 
 if __name__ == "__main__":
 
-	if (len(sys.argv) > 1 ) and any(sys.argv[1] in s for s in ['--help','-h','/?']):
-	    print("Display a list of randomly generated names. \nUsage:\t name_generator.py [-c <number of names>] \n\t -c <number of names>\t Default number of names is five.")
-    	exit(0)
+	# if (len(sys.argv) > 1 ) and any(sys.argv[1] in s for s in ['--help','-h','/?']):
+	#     print("Display a list of randomly generated names. \nUsage:\t name_generator.py [-c <number of names>] \n\t -c <number of names>\t Default number of names is five.")
+ #    	exit(0)
 
-	main()
+	#main()
+	print 'Unicode in console'
+	# print 'Русские буквы'
+	# print(u'\N{snowman}')
